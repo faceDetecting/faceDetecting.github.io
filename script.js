1ff42a209ef3c95f421f1a96ebb730e8ded5eb47
Albert Margaryan <66197687+AlbertMargaryan@users.noreply.github.com>
2020-09-07 11:58:05 +0400

    READY!

---
+++ script.js
@@ -0,0 +1,93 @@
+let outputWidth;
+let outputHeight;
+
+let faceTracker;
+let videoInput;
+
+let imgMask;
+let imgFace;
+let imgFace2;
+let imgFace3;
+
+let selected = -1;
+
+
+function preload() {
+	imgMask = loadImage('mask.png');
+	imgFace1 = loadImage('face.png')
+	imgFace2 = loadImage('face2.png')
+	imgFace3 = loadImage('face3.png')
+}
+
+function setup() {
+	const maxWidth = Math.min(windowWidth, windowHeight);
+	pixelDensity(1)
+	outputHeight = maxWidth * 0.75;
+	outputWidth = maxWidth;
+
+	createCanvas(outputWidth, outputHeight);
+
+	videoInput = createCapture(VIDEO);
+	videoInput.size(outputWidth, outputHeight)
+	videoInput.hide()
+
+	const sel = createSelect();
+	const selList = ['smile', 'Face(Aren)', 'Face(Smbat)', 'Face(Albert)']
+
+	sel.option('Select filter', -1)
+
+	for(let i = 0; i < selList.length; i++) {
+		sel.option(selList[i],i)
+	}
+	sel.changed(applyFilter)
+
+	faceTracker = new clm.tracker();
+	faceTracker.init();
+	faceTracker.start(videoInput.elt)
+}
+function applyFilter() {
+	selected = this.selected();
+}
+
+function draw() {
+	image(videoInput, 0, 0, outputWidth, outputHeight);
+	switch (selected) {
+		case "-1": break;
+		case '0': drawMask(imgMask); break;
+		case '1': drawMask(imgFace1); break;
+		case '2': drawMask(imgFace2); break;
+		case '3': drawMask(imgFace3); break;
+	}
+}
+
+function drawMask(customMask) {
+	const positions = faceTracker.getCurrentPosition();
+	if(positions !== false) {
+		push();
+		const wx = Math.abs(positions[13][0] - positions[1][0]) * 2;
+		const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 2;
+		translate(-wx/2, -wy/2)
+		image(customMask, positions[62][0], positions[62][1], wx, wy);
+		pop();
+	}
+}
+
+// function drawFace() {
+// 	const positions = faceTracker.getCurrentPosition();
+// 	if(positions !== false) {
+// 		push();
+// 		const wx = Math.abs(positions[13][0] - positions[1][0]) * 2;
+// 		const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 2;
+// 		translate(-wx/2, -wy/2)
+// 		image(imgFace, positions[62][0], positions[62][1], wx, wy);
+// 		pop();
+// 	}
+// }
+
+function windowResize() {
+	const maxWidth = Math.min(windowWidth, windowHeight);
+	pixelDensity(1)
+	outputHeight = maxWidth * 0.75;
+	outputWidth = maxWidth;
+	resizeCanvas(outputWidth, outputHeight)
+}
